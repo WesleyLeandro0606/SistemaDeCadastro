1b2fa6addaeb6c97c92f954981c7732ce4b74b2d
fgrthyr <a@a.com>
2026-05-18 20:12:29 -0300

    Initial commit

---
+++ app/src/main/java/br/ulbra/sistemadecadastro/Registro.java
@@ -0,0 +1,24 @@
+package br.ulbra.sistemadecadastro;
+
+public class Registro {
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Registro(String nome, String endereco, String telefone) {
+
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+}
+
